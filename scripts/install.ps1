a60bcfbfc7c1d8907cdda0f6a65b3769825cd207
Xavier Beheydt <xavier.beheydt@gmail.com>
2025-09-28 10:21:50 +0200

    #3 [Deps] Add Path Env function + Cppcheck support

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -6,7 +6,7 @@ $DEPS = @(
     "Microsoft.VisualStudio.2022.BuildTools",
     "KhronosGroup.VulkanSDK",
     "BaldurKarlsson.RenderDoc",
-    # TODO: Add RenderDoc
+    "Cppcheck.Cppcheck"
 )
 
 # Function Declarations
@@ -15,17 +15,31 @@ function Build-CmakeFormat {
 }
 
 function Install-MicrosoftBuildTools {
-        winget install Microsoft.VisualStudio.2022.BuildTools `
-            --force --override "--wait --passive --config $REPO_PATH/configs/.vsconfig"
+    winget install Microsoft.VisualStudio.2022.BuildTools `
+        --force --override "--wait --passive --config $REPO_PATH/configs/.vsconfig"
 }
 
-# TODO: add support to create a VCPKG_ROOT var env
+function Set-PathVar{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Path
+    )
+    $CurrentUserPath = [Environment]::GetEnvironmentVariable('PATH', 'User')
+    if ($CurrentUserPath -notlike "*$Path*") {
+        [Environment]::SetEnvironmentVariable('PATH', "$CurrentUserPath;$Path", 'User')
+    }
+}
 
 # Main part
 foreach ($Dep in $DEPS) {
     Write-Host "Installing $Dep..."
     if ($Dep -like "*BuildTools") {
         Install-MicrosoftBuildTools
+    }
+    elseif ($dep -like "*Cppcheck*") {
+        winget install $Dep
+        Set-PathVar -Path "$env:ProgramFiles\Cppcheck"
     }
     else {
         winget install $Dep
